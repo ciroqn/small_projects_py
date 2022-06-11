621e856e274fdf8f59bf45783029e32afbe2e4d0
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-06-11 12:13:06 +0100

    Create piglatin.py

---
+++ piglatin.py
@@ -0,0 +1,16 @@
+# Pig Latin translator
+
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+# Checks input is actually a word (no non-alphabet characters, which is what .isalpha check for)
+if len(original) > 0 and original.isalpha():
+  word = original.lower()
+  first = word[0]
+  new_word = word[1:len(word)] + first + pyg
+  print new_word
+else:
+  print 'empty'
+  
+# for example 'Crayfish' becomes 'rayfishcay'
