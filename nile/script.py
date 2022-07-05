307d2ea83ebbc34d0afab80d385c9f1a9c568ad9
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-07-05 11:54:55 +0100

    add comments

--- nile/script.py
+++ nile/script.py
@@ -1,19 +1,17 @@
 from nile import get_distance, format_price, SHIPPING_PRICES
 from test import test_function
 
-# Define calculate_shipping_cost() here:
+# Define calculate_shipping_cost(): takes three args, one that is default.
 def calculate_shipping_cost(from_coords, to_coords, shipping_type="Overnight"):
   from_lat, from_long = from_coords
   to_lat, to_long = to_coords
+  # Unpack tuples to get distance between coords
   distance = get_distance(*from_coords, *to_coords)
   shipping_rate = SHIPPING_PRICES[shipping_type]
   price = distance * shipping_rate
   return format_price(price)
 
-# Test the function by calling 
-test_function(calculate_shipping_cost)
-
-# Define calculate_driver_cost() here
+# Define calculate_driver_cost(): finds cheapest driver for the job. The *args takes any number of drivers (each driver being a tuple with speed + salary)
 def calculate_driver_cost(distance, *drivers):
   cheapest_driver = None
   cheapest_driver_price = None
@@ -28,10 +26,7 @@ def calculate_driver_cost(distance, *drivers):
       cheapest_driver_price = price_for_driver
   return cheapest_driver_price, cheapest_driver
 
-# Test the function by calling 
-test_function(calculate_driver_cost)
-
-# Define calculate_money_made() here
+# Define calculate_money_made(): finds total money made by compnay. The 'kwargs' is a dict, with id and trip details
 def calculate_money_made(**trips):
   total_money_made = 0
   for trip_id, trip in trips.items():
@@ -39,5 +34,3 @@ def calculate_money_made(**trips):
     total_money_made += trip_revenue
   return total_money_made
 
-# Test the function by calling 
-test_function(calculate_money_made)
