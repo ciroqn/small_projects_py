8e9801ed0743d08ad3e23f72ceb9db6800c4ba3b
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-07-10 17:22:07 +0100

    Update orion_constellation_map.py
    data from NTU and plotting 2d

--- orion_constellation_map.py
+++ orion_constellation_map.py
@@ -3,3 +3,14 @@
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# Data from NTU (Nottingham Trent Univ) for x, y ,z positions of stars in Orion
+x = [-0.41, 0.57, 0.07, 0.00, -0.29, -0.32,-0.50,-0.23, -0.23]
+y = [4.12, 7.71, 2.36, 9.10, 13.35, 8.13, 7.19, 13.25,13.43]
+z = [2.06, 0.84, 1.56, 2.07, 2.36, 1.72, 0.66, 1.25,1.38]
+
+# 2D scatter of x, y positions of stars
+fig = plt.figure()
+# axes for figure
+fig.add_subplot(1,1,1)
+plt.scatter(x,y)
+plt.show()
