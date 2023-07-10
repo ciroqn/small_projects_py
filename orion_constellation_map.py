42da8b6cd70394df9e93462e460f9e53ef5c8d85
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-07-10 17:19:01 +0100

    Create orion_constellation_map.py

---
+++ orion_constellation_map.py
@@ -0,0 +1,5 @@
+# import tools for 3D scatter plot and rotation tools (which is what %matplotlib allows one to do in jupyter)
+%matplotlib notebook
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
