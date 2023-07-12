b87432cd9da8452066ee01f57243ba6e898fdf3c
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-07-12 14:39:12 +0100

    Create sample_tests_business2.py

---
+++ sample_tests_business2.py
@@ -0,0 +1,56 @@
+# using binomial, ANOVA and Tukey tests for data analysis
+
+import numpy as np
+import fetchmaker as f
+from scipy.stats import binom_test, f_oneway
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
+# get_weight()
+# get_tail_length()
+# get_color()
+# get_age()
+# get_is_rescue()
+
+# rottweiler data
+rottweiler_tl = f.get_tail_length("rottweiler")
+
+print rottweiler_tl
+print np.mean(rottweiler_tl)
+print np.std(rottweiler_tl)
+
+# percentage of whippets being rescue
+whippet_rescue = f.get_is_rescue("whippet")
+
+num_whippet_rescue = np.count_nonzero(whippet_rescue)
+
+print num_whippet_rescue
+
+# number of samples in whippet dataset
+num_whippets = np.size(whippet_rescue)
+
+print num_whippets
+
+# binomial test on whippet rescues in dataset
+whippet_rescue_binom = binom_test(6, num_whippet_rescue, 0.08)
+
+print whippet_rescue_binom
+# 2.62144e-07 (null rejected)
+
+# ANOVA test. weights for three popular dog breeds: whippets, terriers, pitbulls
+whippet_weights = f.get_weight("whippet")
+terrier_weights = f.get_weight("terrier")
+pitbull_weights = f.get_weight("pitbull")
+
+pvalue_weights = f_oneway(whippet_weights, terrier_weights, pitbull_weights)
+
+print pvalue_weights.pvalue
+# 3.27641558827e-17 (null rejected - weights of different breeds not similar, but which pair). requires tukey
+
+# Given ANOVA results, determine Tukey: first concat list and associate el to each datast
+all_weight_data = np.concatenate([whippet_weights, terrier_weights, pitbull_weights])
+labels = ['Whippet'] * len(whippet_weights) + ['Terrier'] * len(terrier_weights) + ['Pitbull'] * len(pitbull_weights)
+
+# Tukey Test: 0.05 is the significance threshold
+tukey_results = pairwise_tukeyhsd(all_weight_data, labels, 0.05)
+
+print tukey_results
