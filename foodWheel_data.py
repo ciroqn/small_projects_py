023afea3dcbfd9f1862403971340361ad499fc6d
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-07-13 15:03:14 +0100

    Update foodWheel_data.py

--- foodWheel_data.py
+++ foodWheel_data.py
@@ -32,3 +32,21 @@ print(avg_order)
 
 # std of price for each month
 std_order = orders.groupby('month').price.std().reset_index()
+
+# bar chart of avg. prices per month
+
+# create axis object
+ax = plt.subplot()
+
+# bar height and errs
+bar_heights = avg_order.price
+bar_errors = std_order.price
+
+
+plt.bar(range(len(bar_heights)), bar_heights, yerr=bar_errors, capsize=5)
+ax.set_xticks(range(len(bar_heights)))
+ax.set_xticklabels(['April', 'May', 'June', 'July', 'August', 'September'])
+plt.title('Avg. Prices / Month')
+plt.xlabel('Month')
+plt.ylabel('Avg. Price')
+plt.show()
