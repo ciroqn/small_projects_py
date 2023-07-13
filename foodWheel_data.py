736fedead4a63ba2c5ec3857306c1ade3cf824ff
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-07-13 14:53:26 +0100

    orders price and std

--- foodWheel_data.py
+++ foodWheel_data.py
@@ -16,3 +16,19 @@ plt.pie(counts, labels=['American', 'Chinese', 'Italian', 'Japanese', 'Korean', 
 plt.title('Most Popular Cuisine Among FoodWheel Partner Restaurants')
 plt.axis('equal')
 plt.show()
+
+# data for orders
+orders = pd.read_csv('orders.csv', delimiter=',')
+
+print(orders.head())
+
+# new column with month
+orders['month'] = orders.date.apply(lambda x: x.split('-')[0])
+
+# avg price for each month
+avg_order = orders.groupby('month').price.mean().reset_index()
+
+print(avg_order)
+
+# std of price for each month
+std_order = orders.groupby('month').price.std().reset_index()
