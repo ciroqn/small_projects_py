ba231e88242357229acf0c0b3db05a86590c00f8
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-11-24 10:57:38 +0000

    add new function

--- retrieval_chatbot/script.py
+++ retrieval_chatbot/script.py
@@ -36,6 +36,25 @@ class ChatBot:
     return responses[response_index]
  
   #define .find_entities():
+  def find_entities(self, user_message):
+    # get tags (parts of speech)
+    tagged_user_message = pos_tag(preprocess(user_message))
+    # get nouns from tagged message (i.e. entities)
+    message_nouns = extract_nouns(tagged_user_message)
+
+    # get word2vec model on nouns by calling word2vec on concatenated nouns
+    tokens = word2vec(" ".join(message_nouns))
+    # get category of blank spot (defined in responses.py)
+    category = word2vec(blank_spot)
+    # get largest similarity
+    word2vec_result = compute_similarity(tokens, category)
+    # sort result by score (note: score is at index 2) in ascending order
+    word2vec_result.sort(key=lambda x: x[2])
+    
+    if len(word2vec_result) > 0:
+      # return first element of last item
+      return word2vec_result[-1][0]
+    return blank_spot
  
   #define .respond():
   def respond(self, user_message):
