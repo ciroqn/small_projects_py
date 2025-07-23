11237229e45044c6faa6efa37cd317450d74d8da
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2025-07-23 19:36:08 +0100

    Create script.py

---
+++ classroom_organiser/script.py
@@ -0,0 +1,29 @@
+from roster import student_roster
+from classroom_organizer import ClassroomOrganizer
+import itertools
+
+student_roster_iter = iter(student_roster)
+
+# for student in student_roster_iter:
+#   print(student)
+
+# instantiate class
+classroom = ClassroomOrganizer()
+
+# print(classroom.student_combinations())
+
+students_maths = classroom.get_students_with_subject('Math')
+students_science = classroom.get_students_with_subject('Science')
+
+# combine above lists
+chain_scimath = itertools.chain(students_maths, students_science)
+
+# get only names from list of tuples
+chain_scimath_names = [el[0] for el in list(chain_scimath)]
+
+# print(chain_scimath_names)
+
+# get all combos [of 4] 
+combos_4 = classroom.student_combinations_4(chain_scimath_names)
+
+print(combos_4)
